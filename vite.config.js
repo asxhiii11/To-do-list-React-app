6dca27a81435890b70967eb7a1e9d81bb357bcc6
asxhiii11 <ashiachu793@gmail.com>
2025-11-04 17:16:27 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/To-do-list-React-app/', 
+  base: "./To-do-list-React-app", 
 })
 
